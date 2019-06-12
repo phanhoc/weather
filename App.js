f38d829b1559809f680a28160c472fbcfe8533df
Phan Van Hoc <hoc.phan.van@nextop.asia>
2019-06-12 16:33:41 +0700

    Create search input component

--- App.js
+++ App.js
@@ -7,43 +7,108 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, ImageBackground} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import SearchInput from'./components/SearchInput'
+import getImageForWeather from './utils/getImageForWeather'
+import {fetchLocationId, fetchWeather} from './utils/api'
 
-type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props)
+    this.state={
+      loading: false,
+      error: false,
+      location:'',
+      temperature:0,
+      weather:'',
+    }
+  }
+
+  handleUpdateLocation = async city => {
+    if (!city) return;
+
+    this.setState({loading: true}, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const {location, weather, temperature} = await fetchWeather(locationId);
+
+        this.setState({
+          loading: false,
+          error: false,
+          location, 
+          weather,
+          temperature,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    });
+  };
+
   render() {
+    const { loading, error, location, weather, temperature } = this.state;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <ImageBackground
+          source={getImageForWeather(weather)}
+          style={styles.imageBackground}
+          imageStyle={styles.image}
+        >
+
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+            <Text style={[styles.smallText, styles.textStyle]}>{weather}</Text>
+            <Text style={[styles.largeText, styles.textStyle]}>{`${Math.round(temperature)}°`}</Text>
+
+            <SearchInput 
+              placeholder="Search any city" 
+              onSubmit={this.handleUpdateLocation}
+            />
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
+  },
+
+  smallText: {
+    fontSize: 18,
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  },
+  imageBackground: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer:{
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
 });
---
+++ componnets/SearchInput.js
@@ -0,0 +1,64 @@
+import Reat from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
+
+export default class SearchInput extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            text:'',
+        };
+    }
+
+    handleChangeText = (newLocation) => {
+        this.setState({text: newLocation});
+    };
+
+    handleSubmitEditing = () =>{
+        const { onSubmit } = this.props
+        const { text } = this.state
+
+        if(!text) return;
+
+        onSubmit(text)
+        this.setState({text:''});
+        
+    };
+
+    render(){
+        const {placeholder} = this.props
+        const {text} = this.state
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    autoCorrect={false}
+                    placeholder={this.props.placeholder}
+                    placeholderTextColor="white"
+                    underlineColorAndroid="transparent"
+                    clearButtonMode="always"
+                    style={styles.textInput}
+                    onChangeText={this.handleChangeText}
+                    value={text}
+                    onSubmitEditing={this.handleSubmitEditing}
+                />
+            </View>
+        );       
+    };
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        height: 40,
+        marginTop: 20,
+        backgroundColor:'#666',
+        marginHorizontal:40,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+
+    textInput:{
+        flex: 1,
+        color: 'white',
+    }        
+});
